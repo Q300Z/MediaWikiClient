9899ff36af9f087563cffb09c712143907ab1a99
Thomas Boutin <34000576+Q300Z@users.noreply.github.com>
2023-11-05 18:00:16 +0100

    Update Constants.cs

--- MediaWikiClient/Constants.cs
+++ MediaWikiClient/Constants.cs
@@ -4,9 +4,9 @@ public class Constants
 {
     public bool IsConfigured { get;  } = Preferences.Get("isconfigured", false);
 
-    public string DbAdresse { get; } = Preferences.Get("dbAdresse", "192.168.1.103");
+    public string DbAdresse { get; } = Preferences.Get("dbAdresse", "localhost");
     public string DbUsername { get; } = Preferences.Get("dbUsername", "sa");
-    public string DbPassword { get;  } = Preferences.Get("dbPassword", "password@123");
+    public string DbPassword { get;  } = Preferences.Get("dbPassword", "yourpassword");
     public string DbName { get; } = Preferences.Get("dbName", "mediawiki");
     public bool TrustServerCertificate { get;  } = Preferences.Get("trustServerCertificate", true);
 
